1af95e2a5eebc28f0fe069e221cafde21b0a7a2b
Roman Maksimov <get.my.email.name@yandex.ru>
2021-09-03 15:37:25 +0300

    remove keys

--- keys/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  BASE_URL: 'http://localhost:3000',
-  MONGODB_URI: 'mongodb+srv://rion:8QF9WJbiMNsvHTjc@cluster0.bdm1j.mongodb.net/shop',
-  SESSION_SECRET: 'some string',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'r.maximov2012@yandex.ru',
-};
